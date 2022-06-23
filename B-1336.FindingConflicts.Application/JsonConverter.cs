c605e8ef5d21c2898c56cd4241f84c594ab245bf
Иван <84615148+INDRA909@users.noreply.github.com>
2022-06-23 22:27:02 +0500

    Init

---
+++ B-1336.FindingConflicts.Application/JsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using B_1336.FindingConflicts.Entities;
+using System.Text.Json;
+
+namespace B_1336.FindingConflicts.Application;
+public class JsonConverter
+{
+    /// <summary>
+    /// Десериализовать DeviceInfo.json
+    /// </summary>
+    public List<DeviceInfo> DeserializeDevices()
+    {
+        using (FileStream file = new FileStream(@"C:\Users\ivanr\OneDrive\Рабочий стол\Devices.json", FileMode.Open))
+        {
+            //List<DeviceInfo>? deviceInfo = JsonSerializer.Deserialize<List<DeviceInfo>>(file);
+            var option = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+            List<DeviceInfo>? deviceInfo = JsonSerializer.Deserialize<List<DeviceInfo>>(file,option);
+            return deviceInfo;
+        }
+    }
+    /// <summary>
+    /// Сереализовать список бригад с использованными каждой бригадой приборами(хотя бы 1 "В сети")
+    /// </summary>
+    public void SerializeToConflicts(List<Conflict> conflicts)
+    {
+        using (FileStream file = new FileStream("Conflict.json", FileMode.OpenOrCreate))
+        {
+            JsonSerializer.Serialize<List<Conflict>>(file, conflicts);
+        }
+    }
+
+}
+
---
+++ B-1336.FindingConflicts.ConsolelLoger/Program.cs
@@ -0,0 +1,8 @@
+﻿using B_1336.FindingConflicts.Application;
+
+var jsonConverter = new JsonConverter();
+var devicesInfo = jsonConverter.DeserializeDevices();
+
+ConflictResult result = new ConflictResult(devicesInfo);
+
+jsonConverter.SerializeToConflicts(result.FindingConflicts());
