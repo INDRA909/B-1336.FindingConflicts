d77d9b8ae421001b4827b4413a8082723ee4480b
Иван <84615148+INDRA909@users.noreply.github.com>
2022-06-23 22:31:47 +0500

    init

--- B-1336.FindingConflicts.ConsolelLoger/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using B_1336.FindingConflicts.Application;
-
-var jsonConverter = new JsonConverter();
-var devicesInfo = jsonConverter.DeserializeDevices();
-
-ConflictResult result = new ConflictResult(devicesInfo);
-
-jsonConverter.SerializeToConflicts(result.FindingConflicts());
